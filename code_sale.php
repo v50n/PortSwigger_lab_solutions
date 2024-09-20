f63979fadc456728e869a46387266c474a0f3eb8
v50n <vson.ng91@gmail.com>
2024-09-20 12:33:19 +0200

    Update code_sale.php

--- code_sale.php
+++ code_sale.php
@@ -1,7 +1,9 @@
 <?PHP 
 echo "Hello";
 echo "start get content";
-// file_get_contents('./index.php');
+// file_get_contents('/web-serveur/ch13/index.php');
 shell_exec('whoami');
+exec('ls');
+file_get_contents('/web-serveur/ch13/index.php');
 echo "end get content";
 ?>
