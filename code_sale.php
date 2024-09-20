2d2b66cdff9bf9cfa2da903513e81596e48fd08a
v50n <vson.ng91@gmail.com>
2024-09-20 13:05:55 +0200

    Update code_sale.php

--- code_sale.php
+++ code_sale.php
@@ -1,11 +1,7 @@
 <?PHP 
 echo "Hello";
 // file_get_contents('/web-serveur/ch13/index.php');
-$index = file_get_contents('http://challenge01.root-me.org/web-serveur/ch13/index.php');
-$config = file_get_contents('http://challenge01.root-me.org/web-serveur/ch13/config.php');
-$fr = file_get_contents('http://challenge01.root-me.org/web-serveur/ch13/fr_lang.php');
+$index = file_get_contents('http://challenge01.root-me.org/web-serveur/ch13/');
 echo "\n content file";
 echo $index;
-echo $config;
-echo $fr;
 ?>
