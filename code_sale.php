0650a12453bc892460b76e229ca39ceab7707b7b
v50n <vson.ng91@gmail.com>
2024-09-20 13:16:39 +0200

    Update code_sale.php

--- code_sale.php
+++ code_sale.php
@@ -4,4 +4,8 @@ echo "Hello";
 $index = file_get_contents('http://challenge01.root-me.org/web-serveur/ch13/');
 echo "\n content file";
 echo $index;
+echo "file test";
+    $myfile = fopen("index.php", "r") or die("Unable to open file!");
+    echo fread($myfile,filesize("index.php"));
+    fclose($myfile);
 ?>
