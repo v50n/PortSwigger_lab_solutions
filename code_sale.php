d6ee2d26520f18160b205c660ec2d35012eab077
v50n <vson.ng91@gmail.com>
2024-09-20 12:49:44 +0200

    Update code_sale.php

--- code_sale.php
+++ code_sale.php
@@ -2,6 +2,6 @@
 echo "Hello";
 // file_get_contents('/web-serveur/ch13/index.php');
 $file = file_get_contents('http://challenge01.root-me.org/web-serveur/ch13/config.php');
-echo "\n content file"
+echo "\n content file";
 echo $file;
 ?>
